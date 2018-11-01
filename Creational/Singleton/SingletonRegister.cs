9aa100d0f1816250d00e5c7d02baa150e9548c5c
Calle <cstenson@kth.se>
2018-11-01 21:42:27 +0100

    Added SingletonPattern

---
+++ Creational/Singleton/SingletonRegister.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SingletonRegister
+{
+    private static SingletonRegister instance;
+    private static List<NameSingletonPair> _registry;
+
+    public static void Register(char name, Singleton s)
+    {
+        //Needs to be registred correctly at startup.
+        //check so not in registry
+        _registry.Add(new NameSingletonPair(name, s));
+    }
+
+    public static Singleton Lookup(char name)
+    {
+        //can be done nicer
+        //var book = books.Where(book => book.author == search).FirstOrDefault();
+        NameSingletonPair sp = _registry.Where(n => n.name == name).FirstOrDefault();
+        return sp.singleton;
+    }
+
+    public static SingletonRegister Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new SingletonRegister();
+            }
+            return instance;
+        }
+    }
+
+
+}
+
+internal class NameSingletonPair
+{
+    internal char name;
+    internal Singleton singleton;
+
+    public NameSingletonPair(char name, Singleton s)
+    {
+        this.name = name;
+        this.singleton = s;
+    }
+}
